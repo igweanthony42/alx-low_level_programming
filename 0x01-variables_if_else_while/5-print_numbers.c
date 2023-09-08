31242e34544572d8a83bd77fced0d92abac4ce9d
Anthony Igwe <igwetony42@gmail.com>
2023-09-08 12:23:09 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+	* main - program that prints the alphabet in lowercase and upper case
+	* Return: Always 0.
+	*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = '0'; letter <= '9'; letter++)
+	putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
+
