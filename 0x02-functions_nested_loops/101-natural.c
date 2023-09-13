cb84454b7ad442dbfcfa37e394ab16a728cd07d3
Anthony Igwe <igwetony42@gmail.com>
2023-09-13 02:24:40 -0700

    multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0 ; i < 1024 ; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
