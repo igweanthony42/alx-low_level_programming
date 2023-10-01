5397d17214ba340d0bde083b23c3a8ab0f5f9d12
Anthony Igwe <igwetony42@gmail.com>
2023-10-01 08:01:45 -0700

    pow recurse

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
   * _pow_recursion - function name
   * @x: parameter 1
@@ -11,13 +12,13 @@ int _pow_recursion(int x, int y)
 {
 
 	if (y < 0)
-        	return (-1);
+		return (-1);
 
 	else if (y == 0)
-        	return (1);
+		return (1);
 
 	else
-        	return (x * _pow_recursion(x, (y - 1)));
+		return (x * _pow_recursion(x, (y - 1)));
 
 	return (0);
 }
