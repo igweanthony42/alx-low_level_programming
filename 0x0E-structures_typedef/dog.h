63988fe004a2782ee8e62963994d1866b9da7832
Anthony Igwe <igwetony42@gmail.com>
2023-10-10 03:45:08 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dogs basic info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
